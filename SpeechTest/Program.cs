438324220c598818b697d71f97826e7c20ccd57c
Steve Taggart <steve.taggart@extendhealth.com>
2019-10-18 16:04:50 -0600

    Transcribe .WAV audio files to text using Azure Speech service.

---
+++ SpeechTest/Program.cs
@@ -0,0 +1,46 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using SpeechTest;
+
+namespace MicrosoftSpeechSDKSamples
+{
+
+
+    class Program
+    {
+        private static string[] GetFiles(string directory)
+        {
+            string[] files = Directory.GetFiles(directory);
+            return files;
+        }
+
+        private static void SaveOutput(string filename, string content)
+        {
+            StreamWriter writer = new StreamWriter(filename);
+            writer.Write(content);
+            writer.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            var finalResponse = new StringBuilder();
+            string[] files = GetFiles(args[0]);
+            foreach (var file in files)
+            {
+                var fileResult = SpeechRecognitionFromFile.ContinuousRecognitionWithFileAsync(file).Result;
+                finalResponse.Append(fileResult);
+                Console.WriteLine("File {0} processed", file);
+            }
+
+            SaveOutput("output.txt", finalResponse.ToString());
+        }
+    }
+}
---
+++ SpeechTest/SpeechRecognitionFromFile.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace SpeechTest
+{
+    class SpeechRecognitionFromFile
+    {
+        public static async Task<string> ContinuousRecognitionWithFileAsync(string fileName)
+        {
+            // <recognitionContinuousWithFile>
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key and service region (e.g., "westus").
+            var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "WestUS2");
+
+            var stopRecognition = new TaskCompletionSource<int>();
+
+            var recognizedText = new StringBuilder();
+
+            // Creates a speech recognizer using file as audio input.
+            // Replace with your own audio file name.
+            using (var audioInput = AudioConfig.FromWavFileInput(fileName))
+            {
+                using (var recognizer = new SpeechRecognizer(config, audioInput))
+                {
+                    // Subscribes to events.
+                    recognizer.Recognizing += (s, e) =>
+                    {
+                        Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
+                    };
+
+                    recognizer.Recognized += (s, e) =>
+                    {
+                        if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                        {
+                            recognizedText.Append(e.Result.Text);
+                            Console.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                        }
+                        else if (e.Result.Reason == ResultReason.NoMatch)
+                        {
+                            Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        }
+                    };
+
+                    recognizer.Canceled += (s, e) =>
+                    {
+                        Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+                        if (e.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+
+                        stopRecognition.TrySetResult(0);
+                    };
+
+                    recognizer.SessionStarted += (s, e) =>
+                    {
+                        Console.WriteLine("\n    Session started event.");
+                    };
+
+                    recognizer.SessionStopped += (s, e) =>
+                    {
+                        Console.WriteLine("\n    Session stopped event.");
+                        Console.WriteLine("\nStop recognition.");
+                        stopRecognition.TrySetResult(0);
+                    };
+
+                    // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
+                    await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+                    // Waits for completion.
+                    // Use Task.WaitAny to keep the task rooted.
+                    Task.WaitAny(new[] { stopRecognition.Task });
+
+                    // Stops recognition.
+                    await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+                }
+            }
+            // </recognitionContinuousWithFile>
+            return recognizedText.ToString();
+        }
+    }
+}
